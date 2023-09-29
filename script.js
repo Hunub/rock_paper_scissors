e9be72eeaf0bdb44a3719891c11c1b064b4c9e3a
Hunub <143363415+Hunub@users.noreply.github.com>
2023-09-29 17:35:40 +1000

    Add check function to keep socre for each match

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ let playerSelectionRaw = prompt("Please enter your choice");
 
 let playerSelection = playerSelectionRaw.toLowerCase();
 
+let totalScore = 0
+
 function capFirstLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -29,10 +31,26 @@ function playRound(a,b) {
         `You lost! ${capFirstLetter(a)} beats ${b}!`;
 }
 
-
-if (!(playerSelection === "rock"||playerSelection === "scissors"||playerSelection === "paper")){
-    alert("Wrong input! Please type rock, paper or scissors.");
-} else {
-    alert(playRound(computerSelection,playerSelection));
+function check(a,b) {
+    if (!(b === "rock"||b === "scissors"||b === "paper")){
+        return "Wrong input! Please type rock, paper or scissors.";
+    } else {
+        let c = playRound(a,b).charAt(4);
+        
+        switch(c){
+            case "w":
+                totalScore++;
+                break;
+            case "l":
+                totalScore--;
+        }
+        
+        return playRound(a,b);
+    }
 }
+
+console.log(totalScore)
+alert(check(computerSelection,playerSelection));
+
+
   
