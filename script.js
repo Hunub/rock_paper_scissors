d032b87fca73e9f1e95e78ea2937c58a60206eb3
Hunub <143363415+Hunub@users.noreply.github.com>
2023-10-13 14:43:19 +1100

    Change win conditions. Instead of a fixe 5 rounds, now the game plays untill a player reaches 5 points.

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let message = "Let's play!";
-console.log(message);
-
 function getComputerChoice() {
     let int= Math.floor(Math.random()*3);
     return int ===0? 
@@ -14,7 +11,8 @@ function getComputerChoice() {
 let playerSelectionRaw;
 let playerSelection;
 let computerSelection;
-let totalScore=0; //must give a numeric value or else totalScore++ would be NaN
+let playerScore = 0;
+let computerScore = 0; 
 
 function capFirstLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -22,55 +20,60 @@ function capFirstLetter(string){
 
 function playRound(a,b) {
     // compare a pair of selections and give a relative result 
-    return a === b?
-        "It's a tie!":
-        (a==="rock"&&b==="paper"||a==="paper"&&b==="scissors"||a==="scissors"&&b==="rock")?
-        `You won! ${capFirstLetter(b)} beats ${a}!`:
-        `You lost! ${capFirstLetter(a)} beats ${b}!`;
+    if(a === b){
+        return "It's a tie!";        
+    }else if(a==="rock"&&b==="paper"||a==="paper"&&b==="scissors"||a==="scissors"&&b==="rock"){
+        playerScore++;
+        return `You won! ${capFirstLetter(b)} beats ${a}!`;
+        
+    }else {
+        computerScore++;
+        return `You lost! ${capFirstLetter(a)} beats ${b}!`;
+        
+    }
 }
 
-function keepScore(a,b) {
-    // give score based on the fifth letter of the string from playRound;
-    //might combine this with playRound using if conditionals?
-        let c = playRound(a,b).charAt(4);
-        switch(c){
-            case "w":
-                totalScore++;
-                break;
-            case "l":
-                totalScore--;
-        }
-        console.log(totalScore);
-        return playRound(a,b);
-}
+// function keepScore(a,b) {
+//     // give score based on the fifth letter of the string from playRound;
+//     //might combine this with playRound using if conditionals?
+//         let c = playRound(a,b).charAt(4);
+//         switch(c){
+//             case "w":
+//                 totalScore++;
+//                 break;
+//             case "l":
+//                 totalScore--;
+//         }
+//         console.log(totalScore);
+//         return playRound(a,b);
+// }
 
 //how to pack this loop into a function?
-let i = 1 
-while (i <= 5) {
+
+while (Math.max(playerScore,computerScore)<5) {
     playerSelectionRaw = prompt("Please enter your choice");
 
     playerSelection = playerSelectionRaw.toLowerCase();
 
-    computerSelection = getComputerChoice(); // random selection needs to be called in each loop
+    computerSelection = getComputerChoice(); 
 
     if (!(playerSelection === "rock"||playerSelection === "scissors"||playerSelection === "paper")){
         alert("Wrong input! Please type rock, paper or scissors."); //make sure input is valid each time
     } else {
-        console.log(computerSelection);
-        console.log(playerSelection);
-        alert(keepScore(computerSelection,playerSelection));
-        i++;
+       console.log(playRound(computerSelection,playerSelection));
+       console.log(playerScore);
+       console.log(computerScore);
     }
 }
 
-function announce(n){
-    return n>0 ?
-    "Yay you won!":
-    n<0 ?
-    "You are unlucky!":
-    "It's still a tie!";
-}
+// function announce(n){
+//     return n>0 ?
+//     "Yay you won!":
+//     n<0 ?
+//     "You are unlucky!":
+//     "It's still a tie!";
+// }
 
-alert(announce(totalScore));
+// alert(announce(totalScore));
 
   
