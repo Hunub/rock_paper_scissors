f600abe2e44fdf834abdbafebd9af39648635171
Hunub <143363415+Hunub@users.noreply.github.com>
2023-10-13 15:19:10 +1100

    Change method of input Added three buttons for player input

--- script.js
+++ script.js
@@ -7,73 +7,58 @@ function getComputerChoice() {
         "scissors";
 }
 
-
-let playerSelectionRaw;
-let playerSelection;
-let computerSelection;
-let playerScore = 0;
-let computerScore = 0; 
-
 function capFirstLetter(string){
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function playRound(a,b) {
-    // compare a pair of selections and give a relative result 
     if(a === b){
         return "It's a tie!";        
     }else if(a==="rock"&&b==="paper"||a==="paper"&&b==="scissors"||a==="scissors"&&b==="rock"){
         playerScore++;
         return `You won! ${capFirstLetter(b)} beats ${a}!`;
-        
     }else {
         computerScore++;
         return `You lost! ${capFirstLetter(a)} beats ${b}!`;
-        
     }
 }
 
-// function keepScore(a,b) {
-//     // give score based on the fifth letter of the string from playRound;
-//     //might combine this with playRound using if conditionals?
-//         let c = playRound(a,b).charAt(4);
-//         switch(c){
-//             case "w":
-//                 totalScore++;
-//                 break;
-//             case "l":
-//                 totalScore--;
-//         }
-//         console.log(totalScore);
-//         return playRound(a,b);
-// }
-
-//how to pack this loop into a function?
-
-while (Math.max(playerScore,computerScore)<5) {
-    playerSelectionRaw = prompt("Please enter your choice");
-
-    playerSelection = playerSelectionRaw.toLowerCase();
-
+function play(){
     computerSelection = getComputerChoice(); 
-
-    if (!(playerSelection === "rock"||playerSelection === "scissors"||playerSelection === "paper")){
-        alert("Wrong input! Please type rock, paper or scissors."); //make sure input is valid each time
-    } else {
-       console.log(playRound(computerSelection,playerSelection));
-       console.log(playerScore);
-       console.log(computerScore);
+    console.log(playRound(computerSelection,playerSelection));
+    console.log(playerScore);
+    console.log(computerScore);
+    if(playerScore === 5 ||computerScore === 5){
+        if(playerScore > computerScore){
+            alert('Yay you won!');
+        }else{
+            alert('You lost :(');
+        }
     }
 }
 
-// function announce(n){
-//     return n>0 ?
-//     "Yay you won!":
-//     n<0 ?
-//     "You are unlucky!":
-//     "It's still a tie!";
-// }
+const rockBtn = document.querySelector('#rock');
+const paperBtn = document.querySelector('#paper');
+const scissorsBtn = document.querySelector('#scissors');
 
-// alert(announce(totalScore));
 
-  
+
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0; 
+
+console.log(rockBtn);
+
+rockBtn.addEventListener('click',() => {
+    playerSelection = 'rock';
+    play();
+    });
+paperBtn.addEventListener('click',() => {
+    playerSelection = 'paper';
+    play();
+    })
+scissorsBtn.addEventListener('click',() => {
+    playerSelection = 'scissors';
+    play();
+    })
