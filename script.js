d1cb93f31128f04cf2304980a8e865a8f59da1c0
Hunub <143363415+Hunub@users.noreply.github.com>
2023-10-17 20:01:15 +1100

    Add image files to repository and other functions Added function to load player and bot display on load so that it can be used to replay

--- script.js
+++ script.js
@@ -13,12 +13,15 @@ function capFirstLetter(string){
 
 function playRound(a,b) {
     if(a === b){
+        announce.textContent = "It's a tie!";
         return "It's a tie!";        
     }else if(a==="rock"&&b==="paper"||a==="paper"&&b==="scissors"||a==="scissors"&&b==="rock"){
         playerScore++;
+        announce.textContent = `You won! ${capFirstLetter(b)} beats ${a}!`;
         return `You won! ${capFirstLetter(b)} beats ${a}!`;
     }else {
         computerScore++;
+        document.querySelector('#result').textContent = `You lost! ${capFirstLetter(a)} beats ${b}!`;
         return `You lost! ${capFirstLetter(a)} beats ${b}!`;
     }
 }
@@ -26,6 +29,9 @@ function playRound(a,b) {
 function play(){
     computerSelection = getComputerChoice(); 
     console.log(playRound(computerSelection,playerSelection));
+    document.querySelector('.player').textContent = playerSelection;
+    document.querySelector('.bot').textContent = computerSelection;
+
     console.log(playerScore);
     console.log(computerScore);
     if(playerScore === 5 ||computerScore === 5){
@@ -41,6 +47,37 @@ const rockBtn = document.querySelector('#rock');
 const paperBtn = document.querySelector('#paper');
 const scissorsBtn = document.querySelector('#scissors');
 
+const announce = document.querySelector('#result');
+announce.style.margin = "auto";
+announce.style.width = "400px";
+
+const show = document.querySelector('#show');
+
+const playerBoard = document.createElement('div');
+playerBoard.setAttribute("class", "player vertical");
+const botBoard =  document.createElement('div');
+botBoard.setAttribute("class", "bot vertical");
+const playerName = document.createElement('h4');
+playerName.setAttribute("class", "name");
+const botName = document.createElement('h4');
+botName.setAttribute("class", "name");
+const playerPic = document.createElement('div');
+const botPic = document.createElement('div');
+
+playerBoard.appendChild(playerName);
+playerBoard.appendChild(playerPic);
+
+botBoard.appendChild(botName);
+botBoard.appendChild(botPic);
+
+
+function setUp(){
+    show.appendChild(playerBoard);
+    show.appendChild(botBoard);
+    return;
+}
+
+
 
 
 let playerSelection;
@@ -48,7 +85,7 @@ let computerSelection;
 let playerScore = 0;
 let computerScore = 0; 
 
-console.log(rockBtn);
+
 
 rockBtn.addEventListener('click',() => {
     playerSelection = 'rock';
